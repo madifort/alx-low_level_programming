7aec0b4f37b721f4bef65c6e129af64ab3618240
Fortune Maduwuike <preshmadu29@gmail.com>
2023-10-05 16:37:22 -0700

    22

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,8 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
+{  
+puts("\"Programming is like building a multilingual puzzle")   
 
-    return (0);
+return (0);
 }
-
