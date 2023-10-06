9cb8777a0656fb6ed9e98168b322d9cd898abc98
Fortune Maduwuike <preshmadu29@gmail.com>
2023-10-05 20:13:50 -0700

    task hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,10 @@
 
 /**
  * main - Entry point of the program
- *
  * Return: Always 0 (success)
  */
 int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);
+{	
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
