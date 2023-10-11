3f7166520ca794d85fdb956d9cb741d3facb97d3
Fortune Maduwuike <preshmadu29@gmail.com>
2023-10-10 19:36:14 -0700

    task o2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -19,19 +19,3 @@ int main(void)
 	return (0);
 
 }
-
-
-
-1 - main.c
-
-#include "main.h"
-
-/**
- * main - check the code
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
