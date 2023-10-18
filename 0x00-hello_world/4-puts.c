685840685254d7cd38a00cb518def39e2bd3252b
Fortune Maduwuike <preshmadu29@gmail.com>
2023-10-18 03:15:52 -0700

    4 betty solution

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point of this program
  *
- * Return: Always 0 (success)
+ * Description: This program prints text using the puts function
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
