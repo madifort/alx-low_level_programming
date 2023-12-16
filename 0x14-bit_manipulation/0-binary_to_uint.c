10d2a7726434f42063356e8cb8dba134cd1bf25e
Fortune Maduwuike <preshmadu29@gmail.com>
2023-12-15 16:37:17 -0800

    betty resolved

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,24 +7,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int t = 0;
-    unsigned int numb = 0;
+int t = 0;
+unsigned int numb = 0;
 
-    if (!b)
-        return 0;
+if (!b)
+	return (0);
 
-    while (b[t] != '\0')
-    {
-        if (b[t] != '0' && b[t] != '1')
-            return 0;
+while (b[t] != '\0')
+{
+	if (b[t] != '0' && b[t] != '1')
+		return (0);
 
-        numb <<= 1;
-        if (b[t] == '1')
-            numb += 1;
+	numb <<= 1;
+	if (b[t] == '1')
+		numb += 1;
 
-        t++;
-    }
+	t++;
+}
 
-    return numb;
+return (numb);
 }
 
